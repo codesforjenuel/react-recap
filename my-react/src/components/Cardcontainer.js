5ed1d8e4b854bd2c0cea850568f06852fcf19dc8
codesforjenuel <codesforjenuel@gmail.com>
2023-04-26 14:23:07 -0700

    updates to card information and responsiveness

--- my-react/src/components/Cardcontainer.js
+++ my-react/src/components/Cardcontainer.js
@@ -4,7 +4,7 @@ const Cardcontainer = () => {
   return (
     <div className='cardContainer'>
       <div className='card'>
-        <img className='cardImg' src='https://blog.logitech.com/wp-content/uploads/2019/08/Keyboard.jpeg' />
+        <img className='cardImg' src='https://ae01.alicdn.com/kf/H35182cd2623841968f4075e8214a71831/LED-Monitor-Lights-Bar-for-Computer-RGB-PC-Monitor-backlight-Stepless-Dimmable-Screen-Hanging-Light-Table.jpg_Q90.jpg_.webp' />
         <ul>
           <li>Model</li>
           <li>Brand</li>
@@ -14,7 +14,7 @@ const Cardcontainer = () => {
         </ul>
       </div>
       <div className='card'>
-        <img className='cardImg' src='https://blog.logitech.com/wp-content/uploads/2019/08/Keyboard.jpeg' />
+        <img className='cardImg' src='https://ae01.alicdn.com/kf/S9fcbe79e41ab42c8a148e5e07f5110bf1/T19-Metal-Luminous-Computer-Keyboard-and-Mouse-Set-USB-Wired-Game-Colorful-Backlight-Mechanical-Feel-Keyboard.jpg_Q90.jpg_.webp' />
         <ul>
           <li>Model</li>
           <li>Brand</li>
@@ -24,7 +24,7 @@ const Cardcontainer = () => {
         </ul>
       </div>
       <div className='card'>
-        <img className='cardImg' src='https://blog.logitech.com/wp-content/uploads/2019/08/Keyboard.jpeg' />
+        <img className='cardImg' src='https://ae01.alicdn.com/kf/Sb378e6dc4c5f4601b6d46d6f4b045729t/ASUSHOM-24-Inch-75hz-curved-Monitor-PC-VA-LCD-HD-Gaming-Monitor-for-Desktop-HDMI-Compatible.jpg_Q90.jpg_.webp' />
         <ul>
           <li>Model</li>
           <li>Brand</li>
