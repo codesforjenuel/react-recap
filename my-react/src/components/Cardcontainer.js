7b2a8d2927bd330f54a6284106d4f1a7b863da86
codesforjenuel <codesforjenuel@gmail.com>
2023-04-27 17:48:56 -0700

    removed the buy now button

--- my-react/src/components/Cardcontainer.js
+++ my-react/src/components/Cardcontainer.js
@@ -4,93 +4,85 @@ const Cardcontainer = () => {
   return (
     <div className='cardContainer'>
       <div className='card'>
-        <img className='cardImg' src='https://ae01.alicdn.com/kf/H35182cd2623841968f4075e8214a71831/LED-Monitor-Lights-Bar-for-Computer-RGB-PC-Monitor-backlight-Stepless-Dimmable-Screen-Hanging-Light-Table.jpg_Q90.jpg_.webp' />
+        <img className='cardImg' src='https://ae01.alicdn.com/kf/H35182cd2623841968f4075e8214a71831/LED-Monitor-Lights-Bar-for-Computer-RGB-PC-Monitor-backlight-Stepless-Dimmable-Screen-Hanging-Light-Table.jpg_Q90.jpg_.webp' alt='for sale item' />
         <ul>
           <li>Model</li>
           <li>Brand</li>
           <li>Item</li>
           <li>$Price</li>
-          <button>Add To Cart</button>
+
         </ul>
       </div>
       <div className='card'>
-        <img className='cardImg' src='https://ae01.alicdn.com/kf/S9fcbe79e41ab42c8a148e5e07f5110bf1/T19-Metal-Luminous-Computer-Keyboard-and-Mouse-Set-USB-Wired-Game-Colorful-Backlight-Mechanical-Feel-Keyboard.jpg_Q90.jpg_.webp' />
+        <img className='cardImg' src='https://ae01.alicdn.com/kf/S9fcbe79e41ab42c8a148e5e07f5110bf1/T19-Metal-Luminous-Computer-Keyboard-and-Mouse-Set-USB-Wired-Game-Colorful-Backlight-Mechanical-Feel-Keyboard.jpg_Q90.jpg_.webp' alt='for sale item' />
         <ul>
           <li>Model</li>
           <li>Brand</li>
           <li>Item</li>
           <li>$Price</li>
-          <button>Add To Cart</button>
         </ul>
       </div>
       <div className='card'>
-        <img className='cardImg' src='https://ae01.alicdn.com/kf/Sb378e6dc4c5f4601b6d46d6f4b045729t/ASUSHOM-24-Inch-75hz-curved-Monitor-PC-VA-LCD-HD-Gaming-Monitor-for-Desktop-HDMI-Compatible.jpg_Q90.jpg_.webp' />
+        <img className='cardImg' src='https://ae01.alicdn.com/kf/Sb378e6dc4c5f4601b6d46d6f4b045729t/ASUSHOM-24-Inch-75hz-curved-Monitor-PC-VA-LCD-HD-Gaming-Monitor-for-Desktop-HDMI-Compatible.jpg_Q90.jpg_.webp' alt='for sale item' />
         <ul>
           <li>Model</li>
           <li>Brand</li>
           <li>Item</li>
           <li>$Price</li>
-          <button>Add To Cart</button>
         </ul>
       </div>
       <div className='card'>
-        <img className='cardImg' src='https://ae01.alicdn.com/kf/Sb378e6dc4c5f4601b6d46d6f4b045729t/ASUSHOM-24-Inch-75hz-curved-Monitor-PC-VA-LCD-HD-Gaming-Monitor-for-Desktop-HDMI-Compatible.jpg_Q90.jpg_.webp' />
+        <img className='cardImg' src='https://ae01.alicdn.com/kf/Sb378e6dc4c5f4601b6d46d6f4b045729t/ASUSHOM-24-Inch-75hz-curved-Monitor-PC-VA-LCD-HD-Gaming-Monitor-for-Desktop-HDMI-Compatible.jpg_Q90.jpg_.webp' alt='for sale item' />
         <ul>
           <li>Model</li>
           <li>Brand</li>
           <li>Item</li>
           <li>$Price</li>
-          <button>Add To Cart</button>
         </ul>
       </div>
       <div className='card'>
-        <img className='cardImg' src='https://ae01.alicdn.com/kf/Sb378e6dc4c5f4601b6d46d6f4b045729t/ASUSHOM-24-Inch-75hz-curved-Monitor-PC-VA-LCD-HD-Gaming-Monitor-for-Desktop-HDMI-Compatible.jpg_Q90.jpg_.webp' />
+        <img className='cardImg' src='https://ae01.alicdn.com/kf/Sb378e6dc4c5f4601b6d46d6f4b045729t/ASUSHOM-24-Inch-75hz-curved-Monitor-PC-VA-LCD-HD-Gaming-Monitor-for-Desktop-HDMI-Compatible.jpg_Q90.jpg_.webp' alt='for sale item' />
         <ul>
           <li>Model</li>
           <li>Brand</li>
           <li>Item</li>
           <li>$Price</li>
-          <button>Add To Cart</button>
         </ul>
       </div>
       <div className='card'>
-        <img className='cardImg' src='https://ae01.alicdn.com/kf/Sb378e6dc4c5f4601b6d46d6f4b045729t/ASUSHOM-24-Inch-75hz-curved-Monitor-PC-VA-LCD-HD-Gaming-Monitor-for-Desktop-HDMI-Compatible.jpg_Q90.jpg_.webp' />
+        <img className='cardImg' src='https://ae01.alicdn.com/kf/Sb378e6dc4c5f4601b6d46d6f4b045729t/ASUSHOM-24-Inch-75hz-curved-Monitor-PC-VA-LCD-HD-Gaming-Monitor-for-Desktop-HDMI-Compatible.jpg_Q90.jpg_.webp' alt='for sale item' />
         <ul>
           <li>Model</li>
           <li>Brand</li>
           <li>Item</li>
           <li>$Price</li>
-          <button>Add To Cart</button>
         </ul>
       </div>
       <div className='card'>
-        <img className='cardImg' src='https://ae01.alicdn.com/kf/Sb378e6dc4c5f4601b6d46d6f4b045729t/ASUSHOM-24-Inch-75hz-curved-Monitor-PC-VA-LCD-HD-Gaming-Monitor-for-Desktop-HDMI-Compatible.jpg_Q90.jpg_.webp' />
+        <img className='cardImg' src='https://ae01.alicdn.com/kf/Sb378e6dc4c5f4601b6d46d6f4b045729t/ASUSHOM-24-Inch-75hz-curved-Monitor-PC-VA-LCD-HD-Gaming-Monitor-for-Desktop-HDMI-Compatible.jpg_Q90.jpg_.webp' alt='for sale item' />
         <ul>
           <li>Model</li>
           <li>Brand</li>
           <li>Item</li>
           <li>$Price</li>
-          <button>Add To Cart</button>
         </ul>
       </div>
       <div className='card'>
-        <img className='cardImg' src='https://ae01.alicdn.com/kf/Sb378e6dc4c5f4601b6d46d6f4b045729t/ASUSHOM-24-Inch-75hz-curved-Monitor-PC-VA-LCD-HD-Gaming-Monitor-for-Desktop-HDMI-Compatible.jpg_Q90.jpg_.webp' />
+        <img className='cardImg' src='https://ae01.alicdn.com/kf/Sb378e6dc4c5f4601b6d46d6f4b045729t/ASUSHOM-24-Inch-75hz-curved-Monitor-PC-VA-LCD-HD-Gaming-Monitor-for-Desktop-HDMI-Compatible.jpg_Q90.jpg_.webp' alt='for sale item' />
         <ul>
           <li>Model</li>
           <li>Brand</li>
           <li>Item</li>
           <li>$Price</li>
-          <button>Add To Cart</button>
         </ul>
       </div>
       <div className='card'>
-        <img className='cardImg' src='https://ae01.alicdn.com/kf/Sb378e6dc4c5f4601b6d46d6f4b045729t/ASUSHOM-24-Inch-75hz-curved-Monitor-PC-VA-LCD-HD-Gaming-Monitor-for-Desktop-HDMI-Compatible.jpg_Q90.jpg_.webp' />
+        <img className='cardImg' src='https://ae01.alicdn.com/kf/Sb378e6dc4c5f4601b6d46d6f4b045729t/ASUSHOM-24-Inch-75hz-curved-Monitor-PC-VA-LCD-HD-Gaming-Monitor-for-Desktop-HDMI-Compatible.jpg_Q90.jpg_.webp' alt='for sale item' />
         <ul>
           <li>Model</li>
           <li>Brand</li>
           <li>Item</li>
           <li>$Price</li>
-          <button>Add To Cart</button>
         </ul>
       </div>
     </div>
